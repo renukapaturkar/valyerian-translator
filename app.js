9dd07fa18cc3046e0440f79a65a8bf54be2c921c
Renuka Paturkar <67357703+renukapaturkar@users.noreply.github.com>
2021-01-17 21:40:38 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output-div");
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" +text;
+}
+
+function errorHandler(error){
+    console.log("error occured! ", error);
+    alert("something wrong with server, try again after some time");
+}
+
+
+
+function clickHandler(){
+    var inputText = textInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => { 
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+
+};
+
+
+btnTranslate.addEventListener("click", clickHandler);
+
+
